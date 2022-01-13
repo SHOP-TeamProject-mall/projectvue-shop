c363e2262ddb89098ef90eb0e0233564bae3c2d0
kwonyongwon <ajasdu@naver.com>
2022-01-13 23:14:48 +0900

    20220113
    주문조회 완성

--- src/components/Mypage.vue
+++ src/components/Mypage.vue
@@ -76,8 +76,69 @@
             <div class="order_title">
                 <img src="@/assets/img/servicecenter.gif" style="width:210px; height:200px; padding:0px; margin:0px;" alt="">
             </div>
-            <div class="ordercontent">
-                이미지 / 상품명 / 주문번호 / 옵션 / 수량 / 판매가 / 배송상태 / 수취인정보
+            <div class="ordercontent" v-if="order_content_btn === true">
+                <span class="orderlist_img">
+                    <img :src="`/HOST/productoption/select_productoption_image.json?productoptionno=${this.order_content.order_productoptionno}`" style="width:100%; height:100%; border-radius:25px; border:3px solid pink; display: inline;" alt="">
+                </span>
+                <span class="orderlist_product_name">
+                    <p>{{this.order_content.productname}}</p><label for="">상품명</label>
+                </span>
+                <span class="orderlist_product_ordernumber">
+                    <p>{{this.order_content.ordernumber}}</p>
+                </span>
+                <span class="orderlist_product_productoption">
+                    <p>{{this.order_content.order_productoptioncolor}} - {{this.order_content.order_productoptionsize}}</p><label for="">옵션</label>
+                </span>
+                <span class="orderlist_product_price">
+                    <p>{{this.order_content.order_amount_paid}}원</p>
+                </span>
+                <span class="orderlist_product_cnt">
+                    <p>{{this.order_content.order_productoptioncnt}}개</p>
+                </span>
+                <span class="orderlist_product_state">
+                    <p>{{this.order_content.delivery_status}}</p>
+                </span> 
+                <span class="orderlist_product_user">
+                    <p>{{this.order_content.reciever_name}}</p><label for="">수취인</label>
+                </span>
+                <span class="orderlist_product_userphone">
+                    <p>{{this.order_content.reciever_phone}}</p><label for="">연락처</label>
+                </span>
+                <span class="orderlist_product_useraddress">
+                    <p>{{this.order_content.reciever_address}}</p><label for="">주소</label>
+                </span>
+            </div>
+            <div class="ordercontent1">
+                <span class="orderlist_img">
+                    <img src="../assets/img/main3.jpg" style="width:100%; height:100%; border-radius:25px; border:3px solid pink; display: inline;" alt="">
+                </span>
+                <span class="orderlist_product_name">
+                    <p></p><label for="">상품명</label>
+                </span>
+                <span class="orderlist_product_ordernumber">
+                    <p></p>
+                </span>
+                <span class="orderlist_product_productoption">
+                    <p></p><label for="">옵션</label>
+                </span>
+                <span class="orderlist_product_price">
+                    <p></p>
+                </span>
+                <span class="orderlist_product_cnt">
+                    <p></p>
+                </span>
+                <span class="orderlist_product_state">
+                    <p></p>
+                </span> 
+                <span class="orderlist_product_user">
+                    <p></p><label for="">수취인</label>
+                </span>
+                <span class="orderlist_product_userphone">
+                    <p></p><label for="">연락처</label>
+                </span>
+                <span class="orderlist_product_useraddress">
+                    <p></p><label for="">주소</label>
+                </span>
             </div>
             <div class="ordertable">
                 <table style="border:1px solid rgb(223, 217, 217); width:1200px; text-align:center;">
@@ -94,24 +155,23 @@
                     <tbody style="border:1px solid rgb(223, 217, 217); height:50px;" v-for="(ordercheck,idx) in orderlist" v-bind:key="ordercheck">
                         <td>{{idx+1}}</td>
                         <td>{{ordercheck.ordernumber}}</td>
-                        <td><a href="#" style="color:black;" @click="ordercontent">{{ordercheck.productname}}</a></td>
+                        <td><a href="#" style="color:black;" @click="ordercontent(ordercheck.ordernumber)">{{ordercheck.productname}}</a></td>
                         <td>{{ordercheck.order_productoptioncolor}} - {{ordercheck.order_productoptionsize}}</td>
                         <td>{{ordercheck.order_productoptioncnt}}</td>
                         <td>{{ordercheck.order_amount_paid}}</td>
                         <td>{{ordercheck.delivery_status}}</td>
                         <td><button type="button" class="btn btn-outline-warning">작성</button></td>
                     </tbody>
-                    <tbody style="border:1px solid rgb(223, 217, 217); height:50px;" v-if="order_content_btn === true">
-                        <td></td>
-                        <td>{{this.orderlist[0].ordernumber}}</td>
-                        <td></td>
-                        <td></td>
-                        <td></td>
-                        <td></td>
-                        <td></td>
-                        <td></td>
-                    </tbody>
                 </table>
+            </div>
+            <div class="pagenation">
+                <ol class="ppagenation">
+                    <li class="ppagenation_First"><button @click="Order_Pagenation(idx = 1)"></button></li>
+                    <li class="ppagenation_Prev"><button @click="Order_Pagenation(idx-10)"></button></li>
+                    <li v-for="(productpagetion,idx) in ppages" v-bind:key="productpagetion"><button  @click="Order_Pagenation(idx+1)" >{{idx+1}}</button></li>
+                    <li class="ppagenation_Next"><button @click="Order_Pagenation(idx+10)"></button></li>
+                    <li class="ppagenation_Last"><button @click="Order_Pagenation(idx = ppages)"></button></li>
+                </ol>
             </div>
         </div>
         <!-- 장바구니 -->
@@ -201,7 +261,10 @@ import axios from "axios";
                 // 주문조회
                 orderlist     : [],
                 idx           : [1,2,3,4,5],
-                order_content_btn : false
+                order_content_btn : false,
+                order_productoption_no : 0,
+                ppages        : "",
+                order_content : ""
 
             }
         },
@@ -218,11 +281,22 @@ import axios from "axios";
             this.cartList()
         },
         methods : {
-            ordercontent(){
+            async ordercontent(ordernumber){
+                console.log(ordernumber);
                 this.order_content_btn = !this.order_content_btn;
+
+                const url = `HOST/order/select_myorderlist_one.json?ordernumber=${ordernumber}`;
+                const headers = { "Content-Type": "application/json" , "token" : this.token };
+                const response = await axios.get(url, { headers:headers });
+                console.log(response);
+                if(response.status === 200){
+                    this.order_content = response.data.orderlist;
+                    console.log(this.order_content);
+                }
             },
             // 주문조회
             async SelectMyOrder(){
+
                 const url = `/HOST/order/select_myorderlist.json?page=${this.page}`;
                 const headers = { "Content-Type": "application/json" , "token" : this.token };
                 const response = await axios.get(url, { headers:headers });
@@ -230,6 +304,24 @@ import axios from "axios";
                 if(response.status === 200){
                     this.orderlist = response.data.list;
                     console.log(this.orderlist, "orderlist");
+
+                    this.ppages = response.data.ppage;
+                    console.log(this.ppages);
+                }
+            },
+            async Order_Pagenation(idx){
+                console.log(idx);
+
+                const url = `/HOST/order/select_myorderlist.json?page=${idx}`;
+                const headers = { "Content-Type": "application/json" , "token" : this.token };
+                const response = await axios.get(url, { headers:headers });
+                console.log(response);
+                if(response.status === 200){
+                    this.orderlist = response.data.list;
+                    console.log(this.orderlist, "orderlist");
+
+                    this.ppages = response.data.ppage;
+                    console.log(this.ppages);
                 }
             },
             // 변경 할 이미지 첨부 여부 확인
@@ -344,7 +436,7 @@ import axios from "axios";
 <style lang="scss" scoped>
 @import 'bootstrap/scss/bootstrap';
 .container1{
-    height: 100vh;
+    height: 110vh;
     align-items: center;
     justify-content: center;
 }
@@ -373,14 +465,125 @@ import axios from "axios";
     width: 980px;
     height: 200px;
     left: 280px;
-    border: 1px solid black;
+    border: 1px solid rgb(223, 217, 217);
 }
+.ordercontent label{
+    position: absolute; left: 5px; top: -25px;
+    font-size: 14px; color: rgb(36, 35, 35);
+    background: pink;
+    font-weight: bold;
+}
+
 .order_title{
     position: absolute;
     width: 210px;
     height: 210px;
     left: 40px;
 }
+.orderlist_img{
+    position: absolute;
+    width: 200px;
+    height: 180px;
+    top: 10px;
+    left: 15px;
+    border-radius: 10px;
+}
+.orderlist_product_name{
+    position: absolute;
+    width: 240px;
+    height: 30px;
+    left: 250px;
+    top: 30px;
+    font-weight: bold;
+}
+.orderlist_product_ordernumber{
+    position: absolute;
+    width: 120px;
+    height: 30px;
+    left: 830px;
+    top: 5px;
+    text-align: right;
+    font-weight: bold;
+}
+.orderlist_product_productoption{
+    position: absolute;
+    width: 200px;
+    height: 30px;
+    left: 250px;
+    top: 90px;
+    font-weight: bold;
+}
+.orderlist_product_price{
+    position: absolute;
+    width: 130px;
+    height: 30px;
+    left: 820px;
+    top: 150px;
+    text-align: right;
+    font-size: 20px;
+    font-weight: bold;
+    color: blue;
+}
+.orderlist_product_cnt{
+    position: absolute;
+    width: 100px;
+    height: 30px;
+    left: 850px;
+    top: 120px;
+    font-weight: bold;
+    text-align: center;
+    font-size: 16px;
+}
+.orderlist_product_state{
+    position: absolute;
+    width: 90px;
+    height: 30px;
+    left: 860px;
+    top: 40px;
+    font-weight: bold;
+    color: red;
+}
+.orderlist_product_user{
+    position: absolute;
+    width: 200px;
+    height: 30px;
+    font-weight: bold;
+    left: 520px;
+    top: 30px;
+}
+.orderlist_product_userphone{
+    position: absolute;
+    width: 200px;
+    height: 30px;
+    left: 520px;
+    top: 90px;
+    font-weight: bold;
+}
+.orderlist_product_useraddress{
+    position: absolute;
+    width: 520px;
+    height: 60px;
+    left: 250px;
+    top: 145px;
+    font-weight: bold;
+}
+
+.ordercontent1{
+    position: absolute;
+    width: 980px;
+    height: 200px;
+    left: 280px;
+    border: 1px solid rgb(223, 217, 217);
+    z-index: -1;
+    font-weight: bold;
+}
+.ordercontent1 label{
+    position: absolute; left: 5px; top: -25px;
+    font-size: 14px; color: rgb(36, 35, 35);
+    background: pink;
+    font-weight: bold;
+}
+
 
 // ====================================================================================================================
 
@@ -546,4 +749,28 @@ a{
 .btn-area1 button:hover{
   color: white;
 }
+
+// 페이지 네이션 ===============================================================================================
+.pagenation {
+    position: relative;
+    top: 800px;
+    left: 66.8%;
+    height: 32px;
+}
+
+.ppagenation {text-align: center; padding: 0 0;}
+.ppagenation li {display: inline-block; vertical-align: middle;}
+.ppagenation li button {color: #898786; display: block; width: 32px; height: 32px; border: none; background: white;}
+.ppagenation li button:hover {color: #ff5a20; text-decoration: underline;}
+.ppagenation li.ppagenation_First button,
+.ppagenation li.ppagenation_Prev button,
+.ppagenation li.ppagenation_Next button,
+.ppagenation li.ppagenation_Last button {overflow: hidden; width: 30px; height: 30px; margin: 0 2px; color: transparent; background: no-repeat center; border: none;}
+.ppagenation li.ppagenation_First button {background-image: url('../assets/img/first.png') ;}
+.ppagenation li.ppagenation_Prev button {background-image: url('../assets/img/left.png') ;}
+.ppagenation li.ppagenation_Next button {background-image: url('../assets/img/right.png') ;}
+.ppagenation li.ppagenation_Last button {background-image: url('../assets/img/last.png') ;}
+.ppagenation li.ppagenation_Prev button {margin-right: 23px; margin-left: 13px;}
+.ppagenation li.ppagenation_Next button {margin-left: 23px; margin-right: 13px;}
+
 </style>
